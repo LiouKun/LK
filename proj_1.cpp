c6cc86a2c44b9b95f96daea0f3c970c19afc10ba
LiouKun <47962403+LiouKun@users.noreply.github.com>
2019-03-13 23:23:12 +0800

    PL1.2.0砍掉重練
    gettoken
    totken type define

--- proj_1.cpp
+++ proj_1.cpp
@@ -1,4 +1,3 @@
-// 禁止用tab 
 # include <iostream>
 # include <string>
 # include <sstream>
@@ -6,49 +5,141 @@
 # include <stdlib.h>
 # include <iomanip>
 using namespace std;
-
+string Get_token() ;
+int Token_type( string&str ) ;
+bool S_expression( string str ) ;
+char separator = '\n' ;
+ 
 int main() {
-  char ch ;
-  int lp = 0, rp = 0 ; // 左括號的數量 右括號的數量 
-  string command_line ; // 命令本人 
-  vector<string> token_group ; // 被分屍的命令 = token 
-  ch = cin.get() ;
+  string token ;
   while ( !cin.eof() ) {
-    if ( ch != ';' ) command_line = command_line + ch ;
-    // 處理括號 開始! 
-    if ( ch == '(' ) {
-      lp++ ; 
-      ch = cin.get() ; // 要用cin.get才能接受到(換行)(空白) 
-      command_line = command_line + ch ;
-      if ( ch == ')' ) lp-- ; // >>()<<  左右括號相連代表: nil  
-      if ( ch == '(' ) lp++ ; // >>((<<  這個字元也是左括號 當然也要算進來 
-    } // if()
-    else if ( ch == ')' ) rp++ ;
-    // 處理括號 結束 
+    token = Get_token();
+    cout << Token_type(Get_token()) << "\n" ;
+  } // while()
+} // main()
 
-    if ( ( lp == rp ) && lp != 0  ) {
-      lp = 0 ;
-      rp = 0 ;
-      cout << "into Get_token \n" ;
-      cout << "commandline: " << command_line << "\n" ;
+string Get_token() { // ¦^¶Ç¤@­Ótoken¦^¥h 
+  string token ;
+  char ch ;
+  ch = separator ;
+  while ( isspace(ch) && !cin.eof() ) { // ¸õ¹L©Ò¦³white-spaces¡Bª½¨ì¸I¨ì¡u«Dwhite-space¡v(©ÎEND-OF-FILE)¬°¤î
+    ch = cin.get() ;
+  } // while()
+  while ( !isspace(ch) && ch != '(' && ch != ')' && ch != '\'' && ch != '"' && ch != ';' ) {
+  // ±q¨º­Ó¡u«Dwhite-space¡v¶}©l "Ä~ÄòÅª¤J"¡Bª½¨ì¸I¨ì¡useparator-¦r¤¸¡v¬°¤î
+    token = token + ch ;
+    ch = cin.get() ;
+  } // while()
+  if ( token != "" ) {
+    separator = ch ;
+    return token ;
+  } // if() 
+  
+  if ( ch == '(' || ch == ')' || ch == '\'' ) {
+    separator = ' ' ;
+    token = token + ch ;
+    return token ;
+  } // if() 
+  else if ( ch == '"' ) { // 
+    separator = ' ' ;
+    token = token + ch ;
+    ch = cin.get() ;
+    while ( ch != '"' ) {
+      if ( ch == '\\' ) {
+        char back_slash = ch ;
+        ch = cin.get() ;
+        if ( ch == 't' ) token = token + '\t' ;
+        else if ( ch == '\\' ) token = token + '\\' ;
+        else if ( ch == 'n' ) token = token + '\n' ;
+        else if ( ch == '"' ) token = token + '\"' ;
+        else token = token + back_slash ;
+      }
+      else  token = token + ch ;
       ch = cin.get() ;
-      command_line = "" ;
-      if ( ch == '\n' ) command_line = "" ;
-      else command_line = command_line + ch ;
-    } // if()
-    else if ( ( ch == '\n' && lp == 0 && rp == 0 ) || ( ch == ';' && command_line != "" ) ) {
-      lp = 0 ;
-      rp = 0 ;
-      cout << "into Get_token \n" ; 
-      cout << "commandline: " << command_line << "\n" ;
-      command_line = "" ;
-      if ( ch == ';' ) {
+      if ( ch == '\n' ) { // ¦r¦ê³B²zªºerror¦b³o¸Ì´N¥i¥H³B²z
+        cout << "error\n" ;
         ch = cin.get() ;
         while ( ch != '\n' ) {
           ch = cin.get() ;
-        } 
+        }
       }
-    } // else if()
-    ch = cin.get() ; 
-  } // while()
-} // main()
+      if ( ch == '"' ) token = token + ch ;
+    } // while() 
+  } // if()
+  else if ( ch == ';' ) {
+    separator = ' ' ;
+    ch = cin.get() ;
+    while ( ch != '\n' ) {
+      ch = cin.get() ;
+    }
+  } // if()
+} // Get_token() 
+
+int Token_type ( string&str ) {   
+  /*
+  1 LEFT-PAREN       // '('
+  2 RIGHT-PAREN      // ')'
+  3 INT              // e.g., '123', '+123', '-123'
+  4 STRING           // "string's (example)." (strings do not extend across lines)
+  5 DOT              // '.'
+  6 FLOAT            // '123.567', '123.', '.567', '+123.4', '-.123'
+  7 NIL              // 'nil' or '#f', but not 'NIL' nor 'nIL'
+  8 T                // 't' or '#t', but not 'T' nor '#T'
+  9 QUOTE            // '
+  10 SYMBOL    */
+  if ( str[0] == '"' && str[str.length()-1] == '"' ) { 
+    str[0] = '\0' ;
+    str[str.length()-1] = '\0' ;
+    return 4 ;
+  }
+  else if ( str == "nil" || str == "#f" ) {
+    str = "nil" ;
+    return 7 ;
+  }
+  else if ( str == "t" || str == "#t" ) {
+    str == "#t" ;
+    return 8 ;
+  }
+  else if ( str == "(" ) {
+    separator = cin.get() ;
+    if ( separator == ')' ) {
+      str == "nil" ;
+      separator = ' ' ;
+      return 7 ;
+    }
+    else return 1 ;
+  }
+  else if ( str == "'" ) {
+    str == "quote" ;
+    return 9 ;
+  }
+  else if ( str == "." ) return 5 ;
+  else if ( str == ")" ) return 2 ;
+  
+  int dot = 0, plus = 0, minus = 0 ;
+  bool isNum = true ;
+  for ( int i = 0 ; i < str.length() ; i++ ) {
+    if ( str[i] == '.' ) dot++ ;
+    else if ( str[i] == '+' ) plus++ ;
+    else if ( str[i] == '-' ) minus++ ;
+    else if ( str[i] < 48 || str[i] > 57 ) isNum = false ;
+  }
+  if ( isNum && ( ( plus == 1 && minus == 0 ) || ( plus == 0 && minus == 1 ) || ( plus == 0 && plus == 0 ) ) && ( dot == 0 || dot == 1 ) ) {
+    if ( dot == 1 ) return 6 ;
+    else return 3 ;
+  }
+  else return 10 ;
+  
+}
+
+bool S_expression( string&str ) {
+  string token ;
+  int token_type = Token_type(str) ;
+  if ( token_type == 3 || token_type == 4 || token_type == 5 || token_type == 6 || token_type == 7 || token_type == 8 || token_type == 10  ) {
+    return true ;
+  } 
+  else if ( token_type == 1 ) {
+    token = Get_token();
+    
+  }
+}
