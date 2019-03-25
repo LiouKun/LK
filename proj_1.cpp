e392b011c5a04123a6167f6a61b008bdc8f1505c
LiouKun <47962403+LiouKun@users.noreply.github.com>
2019-03-25 17:30:45 +0800

    正確的文法判斷3/25

--- proj_1.cpp
+++ proj_1.cpp
@@ -5,7 +5,7 @@
 # include <stdlib.h>
 # include <iomanip>
 using namespace std;
-string Get_token() ;
+string Get_token( bool& ) ;
 int Token_type( string&str ) ;
 bool S_expression( string&str ) ;
 void Cross_out() ;
@@ -15,22 +15,25 @@ struct node {
 	node *left;      // Link to left child
 	node *right;     // Link to right child
 };
-char separator = '\n' ;
+char separator = ' ' ;  // 有待改進的設計 可以改用cin.peek 
 int main() {
-  string token = " " ;
+  bool str_error ; // 字串錯誤，在字串內換行的錯誤 
+  string token = " " ; 
   while ( !cin.eof() ) {
-    token = Get_token() ;
-    while( token == "" ) token = Get_token() ;
-    if ( S_expression( token ) )  cout << "true\n" ;
+    token = Get_token( str_error ) ;
+    while( token == "" ) token = Get_token( str_error ) ; // 一定要get到一個token 
+    if ( !str_error && S_expression( token ) )  cout << "true\n" ;  // 沒有任何錯誤 
     else cout << "error\n" ;
   } // while()
 } // main()
 
-string Get_token() { // 回傳一個token回去 
+string Get_token( bool&str_error ) { // 回傳一個token回去 
+  str_error = false ; // 字串錯誤初始為false 
   string token ;
   char ch ;
-  ch = separator ;
-  while ( isspace(ch) && !cin.eof() ) { // 跳過所有white-spaces、直到碰到「非white-space」(或END-OF-FILE)為止
+  ch = separator ; // 從那個separator字元開始判斷 
+  while ( isspace(ch) && !cin.eof() ) { 
+  // 跳過所有white-spaces、直到碰到「非white-space」(或END-OF-FILE)為止
     ch = cin.get() ;
   } // while()
   while ( !isspace(ch) && ch != '(' && ch != ')' && ch != '\'' && ch != '"' && ch != ';' ) {
@@ -38,23 +41,30 @@ string Get_token() { // 回傳一個token回去
     token = token + ch ;
     ch = cin.get() ;
   } // while()
-  if ( token != "" ) {
+  if ( token != "" ) { 
     separator = ch ;
     return token ;
   } // if() 
   
+  // 以下開始處理分離字元 
   if ( ch == '(' || ch == ')' || ch == '\'' ) {
+    // 碰到這三個 簡單 直接return回去 
     separator = ' ' ;
     token = token + ch ;
     return token ;
   } // if() 
-  else if ( ch == '"' ) { // 
+  else if ( ch == '"' ) { // 字串的處理 
     separator = ' ' ;
     token = token + ch ;
     ch = cin.get() ;
     while ( ch != '"' ) {
-      if ( ch == '\\' ) {
-        char back_slash = ch ;
+      if ( ch == '\n' ) { // 字串處理的error在這裡就可以處理
+        str_error = true ; // 字串error
+        token = "error" ;
+        return token ;
+      }
+      else if ( ch == '\\' ) { // 碰到'\' -> escape字元處理 
+        char back_slash = ch ; 
         ch = cin.get() ;
         if ( ch == 't' ) token = token + '\t' ;
         else if ( ch == '\\' ) token = token + '\\' ;
@@ -64,28 +74,24 @@ string Get_token() { // 回傳一個token回去
       }
       else  token = token + ch ;
       ch = cin.get() ;
-      if ( ch == '\n' ) { // 字串處理的error在這裡就可以處理
-        cout << "error\n" ;
-      }
       if ( ch == '"' ) token = token + ch ;
     } // while() 
   } // if()
-  else if ( ch == ';' ) {
+  else if ( ch == ';' ) { 
     separator = ' ' ;
     ch = cin.get() ;
-    while ( ch != '\n' && !cin.eof() ) {
+    while ( ch != '\n' && !cin.eof() ) { // 讀掉註解後的所有東西 
       ch = cin.get() ;
     }
   } // if()
 } // Get_token() 
-void Cross_out() {
-    char ch ;
-    ch = cin.get() ;
+void Cross_out() { // 劃掉錯誤後的所有東西 
+    char ch = separator ;
+    separator = ' ' ; 
     while ( ch != '\n' ) {
-      cout << "X" ;
       ch = cin.get() ;
+      cout << "X" ;
     } 
-
 }
 int Token_type ( string&str ) {   
   /*
@@ -109,7 +115,7 @@ int Token_type ( string&str ) {
     return 7 ;
   }
   else if ( str == "t" || str == "#t" ) {
-    str == "#t" ;
+    str = "#t" ;
     return 8 ;
   }
   else if ( str == "(" ) {
@@ -118,14 +124,14 @@ int Token_type ( string&str ) {
       separator = cin.get() ;
     }
     if ( separator == ')' ) {
-      str == "nil" ;
+      str = "nil" ;
       separator = ' ' ;
       return 7 ;
     }
     else return 1 ;
   }
   else if ( str == "'" ) {
-    str == "quote" ;
+    str = "quote" ;
     return 9 ;
   }
   else if ( str == "." ) return 5 ;
@@ -146,31 +152,36 @@ int Token_type ( string&str ) {
   else return 10 ;
   
 }
-bool S_expression( string&str ) {
+bool S_expression( string&str ) { // 判斷文法 
   string token ;
-  int token_type = Token_type(str) ;
+  bool str_error ; 
+  int token_type = Token_type(str) ; 
   if ( token_type == 3 || token_type == 4 || token_type == 6 || token_type == 7 || token_type == 8 || token_type == 10  ) {
     return true ;
   }
   else if ( token_type == 1 ) { // LP
-    token = Get_token() ;
-    if ( !S_expression(token) ) {
+    token = Get_token(str_error) ;
+    while( token == "" ) token = Get_token( str_error ) ; // 讀到有東西為止 
+    if ( str_error || !S_expression(token) ) {
       return false ;
     }
     else {
-      token = Get_token() ;
+      token = Get_token(str_error) ;
+      while( token == "" ) token = Get_token( str_error ) ;
       while ( token != "." && token != ")"  )  {
-        if ( !S_expression(token) ) return false ;
-        token = Get_token() ;
+        if ( str_error || !S_expression(token) ) return false ;
+        token = Get_token(str_error) ;
       }
       
       if ( token == ")" ) {
         return true ;
       }
       else if ( token == "." ) {
-        token = Get_token() ;
-        if ( S_expression(token) ) {
-          token = Get_token() ;
+        token = Get_token(str_error) ;
+        while( token == "" ) token = Get_token( str_error ) ;
+        if ( !str_error && S_expression(token) ) {
+          token = Get_token(str_error) ;
+          while( token == "" ) token = Get_token( str_error ) ;
           if ( token != ")" ) {
             Cross_out() ;
             return false ;
@@ -187,8 +198,9 @@ bool S_expression( string&str ) {
 
   }
   else if ( token_type == 9 ) { // quote 
-    token = Get_token() ;
-    if ( !S_expression(token) ) return false ;
+    token = Get_token(str_error) ;
+    while( token == "" ) token = Get_token( str_error ) ;
+    if ( str_error || !S_expression(token) ) return false ;
     else return true ;
   }
   else { // DOT RP 
